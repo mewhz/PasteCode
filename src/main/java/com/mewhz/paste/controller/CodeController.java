fc78cc23034f8d8189d9798dbb481d69dbf7a128
mewhz <mewanghz@163.com>
2022-01-01 01:17:35 +0800

    :white_check_mark:查看代码实现前后端分离

--- src/main/java/com/mewhz/paste/controller/CodeController.java
+++ src/main/java/com/mewhz/paste/controller/CodeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -20,75 +21,13 @@ import java.util.List;
 public class CodeController {
 
     @RequestMapping("/code")
-    public String code(@RequestParam String id) {
-        String htmlFront = "<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <title>";
-        String htmlTitle ="</title>\n" +
-                "    <link href=\"css/prism.css\" rel=\"stylesheet\">\n" +
-                "    <link rel=\"stylesheet\" href=\"css/bootstrap.min.css\"/>\n" +
-                "    <link href=\"https://cdn.bootcdn.net/ajax/libs/toastr.js/latest/css/toastr.min.css\" rel=\"stylesheet\">"+
-                "</head>\n" +
-                "<body>\n" +
-                "    <pre>\n" +
-                "      <code id=\"code\" class=\"language-";
-        String type = null;
-        String text = null;
-        String remark = null;
-        String htmlLast = "      </code>\n" +
-                "    </pre>\n" +
-                "<button id=\"button\" class=\"btn btn-success\" class=\"btn btn-success\" onclick=\"copy()\" data-clipboard-target=\"#code\" data-clipboard-action=\"copy\">一键复制</button>"+
-                "    <script src=\"js/prism.js\"></script>\n"+
-                "<script src=\"js/jquery-3.6.0.min.js\"></script>\n" +
-                "<script src=\"https://cdn.jsdelivr.net/npm/clipboard@2.0.8/dist/clipboard.min.js\"></script>\n"+
-                        "<script src=\"js/code.js\"></script>\n" +
-                        "<script src=\"https://cdn.bootcdn.net/ajax/libs/toastr.js/latest/js/toastr.min.js\"></script>";
-
-        String htmlEnd = "</body>\n" +
-                "</html>";
-        StringBuilder html = new StringBuilder(htmlFront);
-
-        String cHtml = "<script src=\"js/prism-c.min.js\"></script>\n" +
-                        "<script src=\"js/prism-cpp.min.js\"></script>\n";
-        String javaHtml = "<script src=\"js/prism-java.min.js\"></script>\n";
-        String pythonHtml = "<script src=\"js/prism-python.min.js\"></script>\n";
-
-
-        CodeSQL codesql = new CodeSQL();
-        List<Entity> list = codesql.findCode(id);
-
-        type = (String) list.get(0).get("type");
-        text = (String) list.get(0).get("text");
-        remark = (String) list.get(0).get("remark");
-        if ("".equals(remark)){
-            remark = "贴代码";
-        }
-
-        text = text.replaceAll("<", "&lt;");
-        text = text.replaceAll(">", "&gt;");
-        text = text.replaceAll("\n", "<br>");
-        text = text.replaceAll(" ", "&nbsp;");
-
-        html.append(remark).append(htmlTitle).append(type).append("\">").append(text).append(htmlLast);
-        if ("cpp".equals(type)){
-            html.append(cHtml);
-        }
-        else    if ("java".equals(type)){
-            html.append(javaHtml);
-        }
-        else    if ("python".equals(type)){
-            html.append(pythonHtml);
-        }
-        html.append(htmlEnd);
-
-        return html.toString();
+    public ModelAndView code(@RequestParam String id){
+        ModelAndView mav = new ModelAndView("/code.html?id=" + id);
+        return mav;
     }
 
-
-    @RequestMapping("/codes")
-    public JSONObject codes(@RequestParam String id){
+    @RequestMapping("/selectCode")
+    public JSONObject selectCode(@RequestParam String id){
 
         JSONObject json = JSONUtil.createObj();
         String type = null;
@@ -102,16 +41,9 @@ public class CodeController {
         text = (String) list.get(0).get("text");
         remark = (String) list.get(0).get("remark");
 
-//        text = text.replaceAll("<", "&lt;");
-//        text = text.replaceAll(">", "&gt;");
-//        text = text.replaceAll("\n", "<br>");
-//        text = text.replaceAll(" ", "&nbsp;");
-
-
         json.append("text", text);
         json.append("type", type);
         json.append("remark", remark);
-
 
         return json;
     }
--- src/main/resources/static/js/code.js
+++ src/main/resources/static/js/code.js
@@ -1,10 +1,3 @@
-// $(document).ready(()=>{
-//     $("#button").click(()=>{
-//         let text = $("#code").text();
-//         navigator.clipboard.writeText(text);
-//         toastr.success("复制成功，快去粘贴吧！");
-//     });
-// });
 function copy(){
     let clipboard = new ClipboardJS('#button');
     clipboard.on('success', function(e) {
@@ -18,3 +11,45 @@ function copy(){
         toastr.error("一键复制失败，请手动复制！")
     });
 }
+
+function getQueryVariable(variable){
+
+    let query = window.location.search.substring(1);
+    let vars = query.split("&");
+    for (let i = 0; i < vars.length; i ++){
+        let pair = vars[i].split("=");
+        if (pair[0] === variable)  return pair[1];
+    }
+    return false;
+}
+
+$(document).ready(()=>{
+    let id = getQueryVariable("id");
+    const codeId = $("#code");
+    const title = $("title");
+    const body = $("body");
+    $.ajax({
+        url: "/selectCode?id=" + id,
+        success: (result)=>{
+            codeId.attr("class","language-" + result.type);
+            codeId.text(result.text);
+            title.text(result.remark);
+            if (result.remark === "")   {
+                title.text("贴代码");
+            }
+            switch (result.type){
+                case "java":
+                    body.append('<script src="js/prism-java.min.js"></script>');
+                    break;
+                case "cpp":
+                    body.append('<script src="js/prism-c.min.js"></script>');
+                    body.append('<script src="js/prism-cpp.min.js"></script>');
+                    break;
+                case "python":
+                    body.append('<script src="js/prism-python.min.js"></script>');
+            }
+            // 对代码重新渲染
+            Prism.highlightAll();
+        }
+    })
+});
